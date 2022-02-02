cc3b3e0fc34923e1a3dc66a2a95b187223574318
Meor Muhammad Kamil <meorkamil97@gmail.com>
2022-02-02 23:34:32 +0800

    add sudoers

--- ansible/manage.sh
+++ ansible/manage.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-subscription-manager register --username <username> --password <passowrd> --auto-attach
+subscription-manager register --username <username> --password <pass> --auto-attach
+yum install -y git tmux
+echo 'vagrant ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/vagrant
